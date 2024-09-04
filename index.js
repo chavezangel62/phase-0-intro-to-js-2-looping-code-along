4c5562da2e98b46df7a73203451cd4383592247d
angel chavez <chavezangel62@gmail.com>
2024-09-03 21:01:20 -0600

    loops

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+const cards = ["Angel", "Rosa","Adelyn"];
+
+function writeCards(cards) {
+  const messages =[];
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+  return messages;
+}
+writeCards(cards);
+
+function countDown() {
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown);
+    countdown--;
+  }
+}
